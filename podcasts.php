6881990c5e2f6033b97db9a1b0e7ec5627b01140
Balery <023100800c@uandina.edu.pe>
2026-09-24 23:36:27 -0500

    Actualiza iconos de podcasts

--- podcasts.php
+++ podcasts.php
@@ -138,6 +138,10 @@ foreach ($podcasts as $p) {
         rel="stylesheet"
         href="assets/css/style-starter.css"
     >
+     <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    >
 
 
     <style>
